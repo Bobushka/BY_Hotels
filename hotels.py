7361f41bda31b32d5dfd981e9b8f182f0cc3b93d
Bobushka <b.yushenkov@gmail.com>
2025-04-09 18:47:30 +0300

    hotels.py added

---
+++ hotels.py
@@ -0,0 +1,92 @@
+from fastapi import Query, Body, Path, APIRouter
+
+
+router = APIRouter(prefix="/hotels")
+
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Dubai", "name": "dubai"},
+    {"id": 3, "title": "Moscow", "name": "moscow"}
+]
+
+
+@router.get("")
+def get_hotels(
+    id: int | None = Query(default=None, description="Идентификатор отеля"),
+):
+    if id:
+        return [hotel for hotel in hotels if hotel["id"] == id]
+    else:
+        return hotels
+
+
+@router.delete("/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            hotels.remove(hotel)
+            return {"status": "OK"}
+    return {"status": "ERROR"}
+
+
+@router.post("")
+def create_hotel(
+        title: str = Body(embed=True, description="Название отеля"),
+        name: str = Body(embed=True, description="Название отеля на английском"),
+    ):
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title,
+        "name": name
+    })
+    return {"status": "OK"}
+
+
+@router.put("/{hotel_id}")
+def edit_hotel(
+        hotel_id: int = Path(embed=True, description="Идентификатор отеля"),
+        title: str = Body(embed=True),
+        name: str = Body(embed=True)
+    ):
+    """Меняет все параметры одного отеля"""
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            hotel["title"] = title
+            hotel["name"] = name
+            return {"status": "OK"}
+    return {"status": "ERROR. Hotel not found"}
+
+
+@router.patch(
+        path="/{hotel_id}",
+        summary="Меняет один из параметров или оба параметра одного отеля",
+        description="Частично обновляет данные одного отеля"  # можно использовать HTML
+    )
+def update_hotel(
+        hotel_id: int = Path(embed=True, description="Идентификатор отеля"),
+        title: str | None = Body(default=None, embed=True),
+        name: str | None = Body(default=None, embed=True)
+    ):
+    """Меняет один из параметров или оба параметра одного отеля"""
+    if title and name:
+        for hotel in hotels:
+            if hotel["id"] == hotel_id:
+                hotel["title"] = title
+                hotel["name"] = name
+                return {"status": "OK"}
+        return {"status": "ERROR. Hotel not found"}
+    if title: 
+        for hotel in hotels:
+            if hotel["id"] == hotel_id:
+                hotel["title"] = title
+                return {"status": "OK"}
+            return {"status": "ERROR(title). Hotel not found"}
+    if name:
+        for hotel in hotels:
+            if hotel["id"] == hotel_id:
+                hotel["name"] = name
+                return {"status": "OK"}
+            return {"status": "ERROR(name). Hotel not found"}
+    return {"status": "ERROR. At least one parameter must be provided"}
+
--- main.py
+++ main.py
@@ -1,116 +1,13 @@
 from pprint import pprint
 from fastapi import FastAPI, Path, Query, Body
 import uvicorn
-import threading
+
+from hotels import router as router_hotels
 
 
 app = FastAPI()
 
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Dubai", "name": "dubai"},
-    {"id": 3, "title": "Moscow", "name": "moscow"}
-]
-
-# --------------------------------------------------------
-
-import time, asyncio
-
-@app.get("/sync/{id}")
-def sync_func(id: int):
-    print(f"sync. Threads: {threading.active_count()}")
-    print(f"sync.start {id}: {time.time():.2f}")
-    time.sleep(3)
-    print(f"sync.finish {id}: {time.time():.2f}")
-
-
-@app.get("/async/{id}")
-async def async_func(id: int):
-    print(f"async. Threads: {threading.active_count()}")
-    print(f"async. Start {id}: {time.time():.2f}")
-    await asyncio.sleep(3)
-    print(f"async.finish {id}: {time.time():.2f}")
-
-# --------------------------------------------------------
-
-@app.get("/hotels")
-def get_hotels(
-    id: int | None = Query(default=None, description="Идентификатор отеля"),
-):
-    if id:
-        return [hotel for hotel in hotels if hotel["id"] == id]
-    else:
-        return hotels
-
-
-@app.delete("/hotels/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            hotels.remove(hotel)
-            return {"status": "OK"}
-    return {"status": "ERROR"}
-
-
-@app.post("/hotels")
-def create_hotel(
-        title: str = Body(embed=True, description="Название отеля"),
-        name: str = Body(embed=True, description="Название отеля на английском"),
-    ):
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": title,
-        "name": name
-    })
-    return {"status": "OK"}
-
-
-@app.put("/hotels/{hotel_id}")
-def edit_hotel(
-        hotel_id: int = Path(embed=True, description="Идентификатор отеля"),
-        title: str = Body(embed=True),
-        name: str = Body(embed=True)
-    ):
-    """Меняет все параметры одного отеля"""
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            hotel["title"] = title
-            hotel["name"] = name
-            return {"status": "OK"}
-    return {"status": "ERROR. Hotel not found"}
-
-
-@app.patch(
-        path="/hotels/{hotel_id}",
-        summary="Меняет один из параметров или оба параметра одного отеля",
-        description="Частично обновляет данные одного отеля"  # можно использовать HTML
-    )
-def update_hotel(
-        hotel_id: int = Path(embed=True, description="Идентификатор отеля"),
-        title: str | None = Body(default=None, embed=True),
-        name: str | None = Body(default=None, embed=True)
-    ):
-    """Меняет один из параметров или оба параметра одного отеля"""
-    if title and name:
-        for hotel in hotels:
-            if hotel["id"] == hotel_id:
-                hotel["title"] = title
-                hotel["name"] = name
-                return {"status": "OK"}
-        return {"status": "ERROR. Hotel not found"}
-    if title: 
-        for hotel in hotels:
-            if hotel["id"] == hotel_id:
-                hotel["title"] = title
-                return {"status": "OK"}
-            return {"status": "ERROR(title). Hotel not found"}
-    if name:
-        for hotel in hotels:
-            if hotel["id"] == hotel_id:
-                hotel["name"] = name
-                return {"status": "OK"}
-            return {"status": "ERROR(name). Hotel not found"}
-    return {"status": "ERROR. At least one parameter must be provided"}
+app.include_router(router_hotels, tags=["Hotels"])
 
 
 if __name__ == "__main__":
