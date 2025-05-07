6a9443a362ac5ef1d9691a813ac48cf14215b75b
Bobushka <b.yushenkov@gmail.com>
2025-05-07 10:43:08 +0300

    pagination task started

--- hotels.py
+++ hotels.py
@@ -4,12 +4,16 @@ from fastapi import Query, Body, Path, APIRouter
 router = APIRouter(prefix="/hotels")
 
 
+
 hotels = [
     {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Dubai", "name": "dubai"},
-    {"id": 3, "title": "Moscow", "name": "moscow"}
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
 ]
-
 
 @router.get("")
 def get_hotels(
